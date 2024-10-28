e40936805983865142f489b18d30a4dd932c6607
rbadzhanov <rusl94@yandex.ru>
2024-10-28 20:54:13 +0100

    Added students and Courses

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Optional
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import engine, Base, get_db
-from models import Teacher
+from models import Teacher, Course, Student
 from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
@@ -22,6 +22,24 @@ def create_teacher(first_name: str, last_name: str, years_of_experience: int, db
     return jsonable_encoder(db_teacher)
 
 
+@app.post("/teachers/{teacher_id}/courses/")
+def create_course(teacher_id: int, title: str, description: str, db: Session = Depends(get_db)):
+    db_course = Course(title=title, description=description, teacher_id=teacher_id)
+    db.add(db_course)
+    db.commit()
+    db.refresh(db_course)
+    return jsonable_encoder(db_course)
+
+
+@app.post("/students/")
+def create_student(first_name: str, last_name: str, db: Session = Depends(get_db)):
+    db_student = Student(first_name=first_name, last_name=last_name)
+    db.add(db_student)
+    db.commit()
+    db.refresh(db_student)
+    return jsonable_encoder(db_student)
+
+
 # Read a Teacher by ID
 @app.get("/teachers/{teacher_id}")
 def read_teacher(teacher_id: int, db: Session = Depends(get_db)):
@@ -31,11 +49,78 @@ def read_teacher(teacher_id: int, db: Session = Depends(get_db)):
     return jsonable_encoder(db_teacher)
 
 
+# Read a Course by Teacher ID
+@app.get("/teachers/{teacher_id}/courses/")
+def read_courses_by_teacher(teacher_id: int, db: Session = Depends(get_db)):
+    db_course = db.query(Course).filter(Course.teacher_id == teacher_id)
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Courses for this teacher not found")
+    return jsonable_encoder(db_course)
+
+
+@app.get("/courses/{course_id}")
+def read_course(course_id: int, db: Session = Depends(get_db)):
+    db_course = db.query(Course).filter(Course.id == course_id).first()
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    return jsonable_encoder(db_course)
+
+
+@app.get("/students/{student_id}")
+def read_student(student_id: int, db: Session = Depends(get_db)):
+    db_student = db.query(Student).filter(Student.id == student_id).first()
+    if db_student is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+    return jsonable_encoder(db_student)
+
+
+@app.get("/courses/{course_id}/teacher")
+def read_teacher_from_course(course_id: int, db: Session = Depends(get_db)):
+    db_course = db.query(Course).filter(Course.id == course_id).first()
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    db_teacher = db_course.teacher
+
+    return jsonable_encoder(db_teacher)
+
+
+@app.get("/courses/{course_id}/students")
+def read_students_from_course(course_id: int, db: Session = Depends(get_db)):
+    db_course = db.query(Course).filter(Course.id == course_id).first()
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    db_students = db_course.students
+
+    return jsonable_encoder(db_students)
+
+
+@app.get("/students/{student_id}/courses")
+def read_courses_for_student(student_id: int, db: Session = Depends(get_db)):
+    db_student = db.query(Student).filter(Student.id == student_id).first()
+    if db_student is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+    db_courses = db_student.courses
+
+    return jsonable_encoder(db_courses)
+
+
 # Read All Teachers
 @app.get("/teachers/")
 def read_teachers(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     teachers = db.query(Teacher).offset(skip).limit(limit).all()
     return jsonable_encoder(teachers)
+
+
+@app.get("/courses/")
+def read_courses(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    courses = db.query(Course).offset(skip).limit(limit).all()
+    return jsonable_encoder(courses)
+
+
+@app.get("/students/")
+def read_courses(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    courses = db.query(Student).offset(skip).limit(limit).all()
+    return jsonable_encoder(courses)
 
 
 # Update a Teacher
@@ -56,6 +141,50 @@ def update_teacher(teacher_id: int, first_name: Optional[str] = None, last_name:
     return jsonable_encoder(db_teacher)
 
 
+@app.put("/students/{student_id}")
+def update_teacher(student_id: int, first_name: Optional[str] = None, last_name: Optional[str] = None,
+                    db: Session = Depends(get_db)):
+    db_student = db.query(Student).filter(Student.id == student_id).first()
+    if db_student is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+    if first_name is not None:
+        db_student.first_name = first_name
+    if last_name is not None:
+        db_student.last_name = last_name
+    db.commit()
+    db.refresh(db_student)
+    return jsonable_encoder(db_student)
+
+
+@app.put("/courses/{course_id}")
+def update_course(course_id: int, title: Optional[str] = None, description: Optional[str] = None,
+                    db: Session = Depends(get_db)):
+    db_course = db.query(Course).filter(Course.id == course_id).first()
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    if title is not None:
+        db_course.title = title
+    if description is not None:
+        db_course.description = description
+    db.commit()
+    db.refresh(db_course)
+    return jsonable_encoder(db_course)
+
+
+@app.put("/courses/{course_id}/{student_id}")
+def associate_students_with_course(course_id: int, student_id: int, db: Session = Depends(get_db)):
+    db_course = db.query(Course).filter(Course.id == course_id).first()
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    db_student = db.query(Student).filter(Student.id == student_id).first()
+    if db_student is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+    db_course.students.append(db_student)
+    db.commit()
+    db.refresh(db_course)
+    return jsonable_encoder(db_course)
+
+
 # Delete a Teacher
 @app.delete("/teachers/{teacher_id}")
 def delete_teacher(teacher_id: int, db: Session = Depends(get_db)):
@@ -65,5 +194,25 @@ def delete_teacher(teacher_id: int, db: Session = Depends(get_db)):
     db.delete(db_teacher)
     db.commit()
     return {"message": f"Teacher with ID {teacher_id} deleted successfully"}
+
+
+@app.delete("/students/{student_id}")
+def delete_teacher(student_id: int, db: Session = Depends(get_db)):
+    db_student = db.query(Student).filter(Student.id == student_id).first()
+    if db_student is None:
+        raise HTTPException(status_code=404, detail="Student not found")
+    db.delete(db_student)
+    db.commit()
+    return {"message": f"Student with ID {db_student} deleted successfully"}
+
+
+@app.delete("/courses/{course_id}")
+def delete_course(course_id: int, db: Session = Depends(get_db)):
+    db_course = db.query(Course).filter(Course.id == course_id).first()
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    db.delete(db_course)
+    db.commit()
+    return {"message": f"Course with ID {course_id} deleted successfully"}
 
 #Add new endpoints below this line
--- models.py
+++ models.py
@@ -1,5 +1,15 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import relationship
+
 from database import Base
+
+# Association table for the many-to-many relationship
+student_course = Table(
+    'student_course', Base.metadata,
+    Column('student_id', Integer, ForeignKey('courses.id'), primary_key=True),
+    Column('course_id', Integer, ForeignKey('students.id'), primary_key=True)
+)
+
 
 class Teacher(Base):
     __tablename__ = "teachers"
@@ -8,4 +18,25 @@ class Teacher(Base):
     last_name = Column(String, index=True)
     years_of_experience = Column(Integer)
 
+    course = relationship("Course", back_populates="teacher", cascade="all, delete, delete-orphan")
+
+
+class Course(Base):
+    __tablename__ = "courses"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    teacher_id = Column(Integer, ForeignKey('teachers.id'))
+
+    teacher = relationship("Teacher", back_populates="course")
+    students = relationship("Student", secondary=student_course, back_populates="courses")
+
+
+class Student(Base):
+    __tablename__ = "students"
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+
+    courses = relationship("Course", secondary=student_course, back_populates="students")
 #Add the class below this line
